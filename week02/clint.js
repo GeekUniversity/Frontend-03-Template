4c5eda7cb33f1907745610cd20d627e505d20243
wangyao <omidoing@qq.com>
2020-08-12 06:52:14 +0800

    toy browser

---
+++ week02/clint.js
@@ -0,0 +1,90 @@
+
+const net = require('net')
+
+
+class Request{
+    constructor(options){
+        this.method = options.method || "GET"
+        this.host = options.host 
+        this.port = options.port || "8080"
+        this.path = options.path || "/"
+        this.headers = options.headers || {}
+        this.body = options.body || {}
+        if(!this.headers["content-type"]){
+            this.headers["content-type"]="application/x-www-form-urlenconded"
+        }
+        if(this.headers["content-type"]==="application/json"){
+            this.bodyText=JSON.stringify(this.body)
+        }else if(this.headers["content-type"]="application/x-www-form-urlenconded"){
+            this.bodyText=Object.keys(this.body).map(key=>`${key}=${encodeURIComponent(this.body[key])}`).join('&')
+        }
+        this.headers["content-length"]=this.bodyText.length
+    }
+    send(connection){
+        return new Promise((resolve,reject)=>{
+            const parser = new ResponseParser;
+            if(connection){
+                connection.write(this.toString())
+            }else{
+                connection = net.createConnection({
+                    host:this.host,
+                    port:this.port
+                },()=>{
+                    connection.write(this.toString())
+                })
+            }
+            connection.on('data',(data)=>{
+                console.log(data.toString());
+                parser.receive(data.toString())
+                if(parser.isFinshed){
+                    resolve(parser.response)
+                    connection.end()
+                }
+            })
+            connection.on('error',(err)=>{
+                reject(err)
+                connection.end
+            })
+        })
+    }
+    toString(){
+        return `${this.method} ${this.path} HTTP1.1\r
+        ${Object.keys(this.headers).map(key=>{
+            `${key}: ${this.headers[key]}`
+        }).join('\r\n')} \r\r
+        ${this.bodyText}`
+    }
+}
+
+class ResponseParser{
+    constructor(){
+
+    }
+    receive(string){
+        for(let i = 0;i<string.length;i++){
+            this.receiveChar(string.charAt(i))
+        }
+    }
+    receiveChar(char){
+
+    }
+}
+
+
+void async function(){
+    const request = new Request({
+        method:"POST",
+        host:"127.0.0.1",
+        port:"8088",
+        path:"/",
+        headers:{
+            ["X-Foo2"]:"customed"
+        },
+        body:{
+            name:"omiomi"
+        }
+    })
+    const response = await request.send()
+    console.log(response)
+}()
+
