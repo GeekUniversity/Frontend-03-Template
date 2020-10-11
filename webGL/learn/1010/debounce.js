c38c4e9baa0cd48ff439575afd4b4c2e1a5502b5
wangyao <omidoing@qq.com>
2020-10-11 23:43:00 +0800

    282 作业

---
+++ webGL/learn/1010/debounce.js
@@ -0,0 +1,20 @@
+// 函数防抖的实现
+function debounce(fn, wait) {
+    var timer = null;
+  
+    return function() {
+      var context = this,
+        args = arguments;
+  
+      // 如果此时存在定时器的话，则取消之前的定时器重新记时
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+  
+      // 设置定时器，使事件间隔指定事件后执行
+      timer = setTimeout(() => {
+        fn.apply(context, args);
+      }, wait);
+    };
+  }
